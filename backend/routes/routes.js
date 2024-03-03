6045a1ce7b45377aa4dc90039f4943ccea2a31d1
de Breyne <111784301+helio75011@users.noreply.github.com>
2024-03-03 02:33:52 +0100

    get/getId/patch/delete

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -15,20 +15,48 @@ router.post('/post', async (req, res) => {
     }
 })
 
-router.get('/getAll', (req, res) => {
-    res.send('Get All API')
+router.get('/getAll', async (req, res) => {
+    try {
+        const data = await Model.find();
+        res.json(data)
+    } catch(error) {
+        res.status(500).json({message: error.message})
+    }
 })
 
-router.get('/getOne/:id', (req, res) => {
-    res.send(req.params.id)
+router.get('/getOne/:id', async (req, res) => {
+    try {
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    } catch(error) {
+        res.status(500).json({message: error.message})
+    }
 })
 
-router.patch('/update/:id', (req, res) => {
-    res.send('Update by ID API')
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    } catch(error) {
+        res.status(400).json({message: error.message})
+    }
 })
 
-router.delete('/delete/:id', (req, res) => {
-    res.send('Delete by ID API')
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted.`)
+    } catch(error) {
+        res.status(400).json({message: error.message})
+    }
 })
 
 module.exports = router;
